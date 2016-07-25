9a26966f0e561113289083fb19fb4e11a3b6a302
nguyendanghung <nguyendanghung2008@gmail.com>
2016-07-26 00:45:14 +0700

    update

---
+++ src/main/java/com/rangers/jdbctemplate/Application.java
@@ -0,0 +1,47 @@
+package com.rangers.jdbctemplate;
+
+import javax.sql.DataSource;
+import org.apache.commons.dbcp.BasicDataSource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.rangers.jdbctemplate.model.dao", "com.rangers.jdbctemplate.model", "com.rangers.jdbctemplate.model.service"})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+
+//    EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+//    EmbeddedDatabase db =
+//        builder.setType(EmbeddedDatabaseType.).setSeparator(";;")
+//        .addScript("database/create-db.sql")
+//        .addScript("database/insert-data.sql")
+//        .build();
+//    return db;
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("org.apache.phoenix.jdbc.PhoenixDriver");
+        dataSource.setUrl("jdbc:phoenix:52.208.47.78");
+        return dataSource;
+
+    }
+
+    /*
+  @Bean
+  public Jackson2ObjectMapperBuilder objectMapperBuilder() {
+      Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+      builder.serializerByType(PagedList.class, new PagedListSerializer());
+      builder.serializerByType(Employee.class, new EmployeeSerializer());
+      return builder;
+  }*/
+}
