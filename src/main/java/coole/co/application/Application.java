c01a6bef3796b9ecea697590f8428642008f806e
nguyendanghung <nguyendanghung2008@gmail.com>
2016-07-26 14:58:50 +0700

    update

--- src/main/java/coole/co/application/Application.java
+++ src/main/java/coole/co/application/Application.java
@@ -2,19 +2,20 @@ package coole.co.application;
 
 import javax.sql.DataSource;
 import org.apache.commons.dbcp.BasicDataSource;
-//import org.apache.commons.dbcp.BasicDataSource;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
+@Configuration
+@PropertySource(value = {"classpath:phoenix.properties"})
 @SpringBootApplication
-//@ComponentScan(basePackages = {"com.rangers.jdbctemplate.model.dao", "com.rangers.jdbctemplate.model", "com.rangers.jdbctemplate.model.service"})
 public class Application {
+    @Autowired
+    private Environment env;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -23,17 +24,16 @@ public class Application {
     @Bean
     public DataSource dataSource() {
 
-/*    EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        /*    EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
     EmbeddedDatabase db =
         builder.setType(EmbeddedDatabaseType.H2).setSeparator(";;")
         .addScript("database/create-db.sql")
         .addScript("database/insert-data.sql")
         .build();
     return db; */
-
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName("org.apache.phoenix.jdbc.PhoenixDriver");
-        dataSource.setUrl("jdbc:phoenix:ip-172-31-31-71.eu-west-1.compute.internal");
+        dataSource.setUrl(env.getProperty("phoenix.url"));
         return dataSource;
 
     }
