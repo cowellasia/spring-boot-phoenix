3eb51403ac7eb414fd0abd873c50ea4162ca8901
nguyendanghung <nguyendanghung2008@gmail.com>
2016-07-26 22:00:50 +0700

    update

--- src/main/java/coole/co/data/model/dao/EmployeeDaoImpl.java
+++ src/main/java/coole/co/data/model/dao/EmployeeDaoImpl.java
@@ -19,100 +19,99 @@ import org.springframework.stereotype.Repository;
 
 import coole.co.data.model.Employee;
 import coole.co.data.model.PagedList;
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Repository
 public class EmployeeDaoImpl implements IEmployeeDao {
 
-    private JdbcTemplate jdbcTemplate;
+  private JdbcTemplate jdbcTemplate;
 
-    @Autowired
-    public void setDataSource(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
-    }
+  @Autowired
+  public void setDataSource(DataSource dataSource) {
+    this.jdbcTemplate = new JdbcTemplate(dataSource);
+  }
 
-    @Override
-    public List<Employee> getDepartmentEmployees(int departmentId) {
-        System.out.println("Retriving all employees within departmernt :" + departmentId);
+  @Override
+  public List<Employee> getDepartmentEmployees(int departmentId) {
+    System.out.println("Retriving all employees within departmernt :" + departmentId);
 
-        String selectEmpSql = "SELECT id, name, email, department_id FROM employees WHERE department_id = ?";
-        List<Employee> employees = jdbcTemplate.query(selectEmpSql, new Integer[]{departmentId}, new EmplloyeeMapper());
-        return new PagedList<Employee>(employees, 22); //TODO: total records count
-    }
+    String selectEmpSql = "SELECT id, name, email, department_id FROM employees WHERE department_id = ?";
+    List<Employee> employees = jdbcTemplate.query(selectEmpSql, new Integer[] {departmentId}, new EmplloyeeMapper());
+    return new PagedList<Employee> (employees, 22); //TODO: total records count
+  }
 
-    @Override
-    public Employee getEmployee(int employeeId) {
-        System.out.println("Select employee with id :" + employeeId);
+  @Override
+  public Employee getEmployee(int employeeId) {
+    System.out.println("Select employee with id :" + employeeId);
 
-        String selectEmpSql = "SELECT id, name, email, department_id FROM employees WHERE id = ?";
-        Employee employee = jdbcTemplate.queryForObject(selectEmpSql, new Integer[]{employeeId}, new EmplloyeeMapper());
-        return employee;
-    }
+    String selectEmpSql = "SELECT id, name, email, department_id FROM employees WHERE id = ?";
+    Employee employee = jdbcTemplate.queryForObject(selectEmpSql, new Integer[] {employeeId}, new EmplloyeeMapper());
+    return employee;
+  }
 
-    @Override
-    public Employee addEmployee(final Employee employee) {
+  @Override
+  public Employee addEmployee(final Employee employee) {
 //      try {
-        System.out.println("Add an employees to departmernt :" + employee.getDepartmentId());
-       // jdbcTemplate.update("upsert into employees values (name, email, department_id) values(" + employee.getName() + "," + employee.getEmailId() + ",)");
+          System.out.println("Add an employees to departmernt :" + employee.getDepartmentId());
+           // jdbcTemplate.update("upsert into employees values (name, email, department_id) values("+employee.getName()+","+employee.getEmailId()+",)");
 //          Connection connection = jdbcTemplate.getDataSource().getConnection();
 //          Statement statement = connection.createStatement();
 //          statement.executeUpdate("upsert into employees values (1,'Hello World')");
 
 
-        PreparedStatementCreator ps = new PreparedStatementCreator() {
-            @Override
-            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
-                final PreparedStatement ps =
-                        connection.prepareStatement("upsert INTO employees (id, name, email, department_id) VALUES (?,?, ?, ?)");
-                ps.setInt(1, employee.getId());
-                ps.setString(2, employee.getName());
-                ps.setString(3, employee.getEmailId());
-                ps.setInt(4, employee.getDepartmentId());
-                return ps;
-            }
-        };
+          PreparedStatementCreator ps = new PreparedStatementCreator() {
+          @Override
+          public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+          final PreparedStatement ps =
+          connection.prepareStatement("upsert INTO employees (name, email, department_id) VALUES (?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+          ps.setString(1, employee.getName());
+          ps.setString(2, employee.getEmailId());
+          ps.setInt(3, employee.getDepartmentId());
+          return ps;
+          }
+          };
 
-        KeyHolder holder = new GeneratedKeyHolder();
-        jdbcTemplate.update(ps, holder);
+          KeyHolder holder = new GeneratedKeyHolder();
+          jdbcTemplate.update(ps, holder);
 
-        employee.setId(holder.getKey().intValue());
+          employee.setId(holder.getKey().intValue());
 
 
-        return employee;
+
+          return employee;
 //      } catch (SQLException ex) {
 //          Logger.getLogger(EmployeeDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
 //          return null;
 //      }
+  }
+
+  @Override
+  public Employee updateEmployee(Employee employee) {
+    System.out.println("Update an employees for employeeId:" + employee.getId());
+
+    String deleteEmpSql = "UPDATE employees SET name = ?, email = ?, department_id = ? WHERE id = ?";
+    jdbcTemplate.update(deleteEmpSql, employee.getName(), employee.getEmailId(), employee.getDepartmentId(), employee.getId());
+    return employee;
+  }
+
+  @Override
+  public void deleteEmployee(int employeeId) {
+    System.out.println("Delete an employees with employeeId:" + employeeId);
+
+    String deleteEmpSql = "DELETE FROM employees WHERE id = ?";
+    jdbcTemplate.update(deleteEmpSql, employeeId);
+  }
+
+  private static final class EmplloyeeMapper implements RowMapper<Employee> {
+    public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+      Employee employee = new Employee();
+      employee.setId(rs.getInt("id"));
+      employee.setName(rs.getString("name"));
+      employee.setEmailId(rs.getString("email"));
+      employee.setDepartmentId(rs.getInt("department_id"));
+      return employee;
     }
-
-    @Override
-    public Employee updateEmployee(Employee employee) {
-        System.out.println("Update an employees for employeeId:" + employee.getId());
-
-        String deleteEmpSql = "UPDATE employees SET name = ?, email = ?, department_id = ? WHERE id = ?";
-        jdbcTemplate.update(deleteEmpSql, employee.getName(), employee.getEmailId(), employee.getDepartmentId(), employee.getId());
-        return employee;
-    }
-
-    @Override
-    public void deleteEmployee(int employeeId) {
-        System.out.println("Delete an employees with employeeId:" + employeeId);
-
-        String deleteEmpSql = "DELETE FROM employees WHERE id = ?";
-        jdbcTemplate.update(deleteEmpSql, employeeId);
-    }
-
-    private static final class EmplloyeeMapper implements RowMapper<Employee> {
-        public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
-            Employee employee = new Employee();
-            employee.setId(rs.getInt("id"));
-            employee.setName(rs.getString("name"));
-            employee.setEmailId(rs.getString("email"));
-            employee.setDepartmentId(rs.getInt("department_id"));
-            return employee;
-        }
-    }
+  }
 
 }
